2ad9e46b015ceefe6bf036b001d0270a7d8eeed5
jarvis2603 <57632424+jarvis2603@users.noreply.github.com>
2026-07-12 18:34:58 +0700

    Register robot control node

--- ros2_ws/src/moebius_base_driver/setup.py
+++ ros2_ws/src/moebius_base_driver/setup.py
@@ -7,7 +7,7 @@ package_name = 'moebius_base_driver'
 
 setup(
     name=package_name,
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
@@ -19,11 +19,12 @@ setup(
     zip_safe=True,
     maintainer='jarvis2603',
     maintainer_email='jarvis2603@users.noreply.github.com',
-    description='ROS 2 Jazzy mecanum serial driver for MoebiusRobot.',
+    description='ROS 2 Jazzy mecanum serial driver and web supervisor for MoebiusRobot.',
     license='MIT',
     entry_points={
         'console_scripts': [
             'base_driver = moebius_base_driver.base_driver_node:main',
+            'robot_control = moebius_base_driver.robot_control_node:main',
         ],
     },
 )
